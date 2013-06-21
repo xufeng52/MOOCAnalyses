f30904582d151090c62c203893d2e4096375cd32
Feng Xu <xu004@cougars.csusm.edu>
2013-06-21 16:03:51 -0700

    Revert "1.3.5"
    This reverts commit a57487492b8dcb8de33ea18754f167494f1de66d.

--- Khan Academy/Scraping-data_v1.3.5/appengine_config.py
+++
@@ -1,8 +0,0 @@
-from gaesessions import SessionMiddleware
-
-COOKIE_KEY='12345678901234567890123456789012345'
-def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = SessionMiddleware(app, cookie_key=COOKIE_KEY)
-    app = recording.appstats_wsgi_middleware(app)
-    return app
